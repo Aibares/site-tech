8917d7b90b9209d473e47c6c128fefaca550e261
Aibares <132648647+Aibares@users.noreply.github.com>
2023-05-14 22:47:59 +0600

    main

--- server/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: process.env.DATABASE_URI,
-    name: process.env.DATABASE_NAME
-}
--- src/admin/templates/icons.template.js
+++
@@ -1,3 +0,0 @@
-export function renderIcons(icons) {
-    return icons.map(icon => `<option value='${icon}' data-icon="../img/service-icons/${icon}">${icon}</option>`).join('');
-}
